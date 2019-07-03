6c0fc6471f56b6238c107b5a8603744f7a7f0653
zako16 <meerim.zhanuzakova@gmail.com>
2019-07-03 14:17:33 +0600

    added to go payment page steps

--- test/pages/ticket.po.js
+++ test/pages/ticket.po.js
@@ -14,6 +14,15 @@ Ticket.prototype = Object.create({}, {
     addToCartButton: {get: function(){ return $('button[title="Add to Shopping Cart"]'); }},
     addToCartInfoModal: {get: function(){ return $('div#add_to_cart_content'); }},
     goToCartButton: {get: function(){ return this.addToCartInfoModal.$('div.buttons > a:nth-child(2)'); }},
+    removeButton: {get: function(){ return $('a[title="Remove"]'); }},
+    goToCheckoutPageButton: {get: function(){ return $('button[onclick="goToCheckoutpage();"]'); }},
+    emptyCartDiv: { get: function(){ return $('div#js-emptycart'); }},
+    firstnameInput: { get: function(){ return $('input[name="firstname"]'); }},
+    lastnameInput: { get: function(){ return $('input[name="lastname"]'); }},
+    emailInput: { get: function(){ return $('input[name="email"]'); }},
+    emailConfirmInput: { get: function(){ return $('input[name="email_confirm"]'); }},
+    saveAddressButton: { get: function(){ return $('div#sc_address_save'); }},
+    placeOrderButton: { get: function(){return $('div#sc_place_order')}},
 });
 
 module.exports = Ticket;
--- test/specs/ticket.spec.js
+++ test/specs/ticket.spec.js
@@ -3,11 +3,15 @@ const url = require('url');
 
 describe('Dashboard account tab', function () {
     var ticket;
+    const checkoutPageUrl = '/checkoutsimple/onepage/index/step/address';
+    const cartUrl = '/checkout/cart/index';
+    const paymentUrl = '/checkoutsimple/onepage/index/step/payment';
+    const user = {name: 'Hermione', surname: 'Granger', email: 'hermione.granger@magic.world', phone: '19091979'};
 
     beforeEach(function () {
         ticket = new Ticket();
     });
-    
+
     it("click to next button of slider", function () {
         if (ticket.sliderBullets.length > 1) {
             ticket.sliderNext.click();
@@ -33,7 +37,66 @@ describe('Dashboard account tab', function () {
         ticket.addToCartButton.click();
         ticket.addToCartInfoModal.waitForDisplayed(4000);
         ticket.goToCartButton.click();
-        const cartUrl = '/checkout/cart/index';
         expect(url.parse(browser.getUrl()).pathname).toBe(cartUrl);
+    });
+
+    it("check ticket, go to proceed page", function () {
+        ticket.day.click();
+        ticket.selectAdultCount.click();
+        browser.pause(500);
+        ticket.selectAdultCount.selectByIndex(2);
+        ticket.addToCartButton.click();
+        ticket.addToCartInfoModal.waitForDisplayed(4000);
+        ticket.goToCartButton.click();
+        if (url.parse(browser.getUrl()).pathname === cartUrl){
+            ticket.goToCheckoutPageButton.click();
+            expect(url.parse(browser.getUrl()).pathname).toBe(checkoutPageUrl);
+        }
+    });
+
+    it("check ticket, go to proceed page and fill user data", function () {
+        ticket.day.click();
+        ticket.selectAdultCount.click();
+        browser.pause(500);
+        ticket.selectAdultCount.selectByIndex(2);
+        ticket.addToCartButton.click();
+        ticket.addToCartInfoModal.waitForDisplayed(4000);
+        ticket.goToCartButton.click();
+        if (url.parse(browser.getUrl()).pathname === cartUrl){
+            ticket.goToCheckoutPageButton.click();
+            ticket.firstnameInput.click();
+            ticket.firstnameInput.setValue(user.name);
+            ticket.lastnameInput.click();
+            ticket.lastnameInput.setValue(user.surname);
+            ticket.emailInput.click();
+            ticket.emailInput.setValue(user.email);
+            ticket.emailConfirmInput.click();
+            ticket.emailConfirmInput.setValue(user.email);
+            expect(url.parse(browser.getUrl()).pathname).toBe(checkoutPageUrl);
+        }
+    });
+
+    it("check ticket, go to proceed page, fill user data and go to payment page", function () {
+        ticket.day.click();
+        ticket.selectAdultCount.click();
+        browser.pause(500);
+        ticket.selectAdultCount.selectByIndex(2);
+        ticket.addToCartButton.click();
+        ticket.addToCartInfoModal.waitForDisplayed(4000);
+        ticket.goToCartButton.click();
+        if (url.parse(browser.getUrl()).pathname === cartUrl){
+            ticket.goToCheckoutPageButton.click();
+            ticket.firstnameInput.click();
+            ticket.firstnameInput.setValue(user.name);
+            ticket.lastnameInput.click();
+            ticket.lastnameInput.setValue(user.surname);
+            ticket.emailInput.click();
+            ticket.emailInput.setValue(user.email);
+            ticket.emailConfirmInput.click();
+            ticket.emailConfirmInput.setValue(user.email);
+            ticket.saveAddressButton.click();
+            ticket.placeOrderButton.waitForDisplayed();
+            expect(url.parse(browser.getUrl()).pathname).toBe(paymentUrl);
+        }
     });
 });
